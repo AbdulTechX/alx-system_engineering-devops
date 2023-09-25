a81673665230807f6043f5dbd746c544650e3f41
AbdulTechX <abdulazeezbuhari27@gmail.com>
2023-09-25 02:49:49 -0700

    config

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# Installs puppet-lint
-
-package { 'python3-flask':
-  ensure   => '2.1.1',
-  provider => 'gem',
+# Using Puppet, install flask from pip3
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
